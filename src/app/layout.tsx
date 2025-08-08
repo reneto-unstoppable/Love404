49597e9d73c88051a84f5f6cb394ef9c423ae701
Reneto Noble <renetoman70@gmail.com>
2025-08-08 13:41:13 +0000

    💔 Added more useless chaos to 404 Dating

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Literata:ital,opsz,wght@0,7..72,400;0,7..72,700;1,7..72,400&family=Space+Grotesk:wght@400;700&display=swap" rel="stylesheet" />
       </head>
-      <body className="font-body antialiased">
+      <body className="font-body antialiased" suppressHydrationWarning>
         <AppProviders>
           {children}
         </AppProviders>
